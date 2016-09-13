f1e069ae3fa7e2f0bae0a2449f44b4cf3b7e1a83
teddycoleman <teddy.f.coleman@gmail.com>
2016-09-13 16:41:16 -0700

    adds work for the homework

--- script.js
+++ script.js
@@ -13,9 +13,9 @@ function daysLeft() {
     var a = $( "#datepicker" ).datepicker('getDate').getTime();
     var b = today.getTime();
     var c = 24*60*60*1000;
-    var diffDays = Math.round((a - b)/c);
+    var diffDays = Math.floor((a - b)/c) + 1;
 
-    resultString = ("<p>You have " + diffDays + " days left!");
+    resultString = createResultString(diffDays);
 }
 
 $("#datepicker").on("change", function(){
@@ -35,5 +35,18 @@ $("#datepicker").on("change", function(){
 // condition 5:  there is NO input and the day is tomorrow
 // condition 6:  there is NO input and the date is in the future.
 
-
+function createResultString(numDays){
+	if(numDays > 1){
+		return "<p>You have " + numDays + " days left!</p>";
+	}
+	else if (numDays == 1){
+		return "<p>That's tomorrow!</p>";
+	}	
+	else if (numDays == 0){
+		return "<p>That's today!</p>";
+	}	
+	else if (numDays < 0){
+		return "<p>That date already passed!</p>";
+	}
+}
 
